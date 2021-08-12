09416f055c4737a1c3598695d4f22331e88a14fc
wand2016 <d19921207@gmail.com>
2021-08-12 15:03:25 +0900

    機能実現

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,34 @@
-console.log("Hello World!a");
+const floydSteinberg = require("floyd-steinberg");
+const { tenjify } = require("tenjify");
 
 // twitch chat width is 30-character-width.
 // braille has 2 dots a character.
 const WIDTH = 60;
 
-function getCanvas(): HTMLCanvasElement {
-  return document.querySelector("#canvas") as HTMLCanvasElement;
+type Resolution = {
+  width: number;
+  height: number;
+};
+
+function getCanvasPreviewRaw(): HTMLCanvasElement {
+  return document.querySelector("#preview-raw") as HTMLCanvasElement;
+}
+function getCanvasPreviewDithered(): HTMLCanvasElement {
+  return document.querySelector("#preview-dithered") as HTMLCanvasElement;
 }
 
-function computeResolution(image: HTMLImageElement) {
+function computeResolution(image: HTMLImageElement): Resolution {
   return {
     width: WIDTH,
     height: Math.round((WIDTH * image.height) / image.width),
   };
 }
 
-async function putImageToCanvas(src: string, canvas: HTMLCanvasElement) {
-  const image = await loadImage(src);
-
-  const resolution = computeResolution(image);
-
+async function putImageToCanvas(
+  image: CanvasImageSource,
+  canvas: HTMLCanvasElement,
+  resolution: Resolution
+) {
   canvas.width = resolution.width;
   canvas.height = resolution.height;
 
@@ -45,11 +54,32 @@ function loadImage(src: string): Promise<HTMLImageElement> {
 }
 
 async function onUpload(this: HTMLInputElement) {
-  const src = URL.createObjectURL(this.files[0]);
+  const image = await loadImage(URL.createObjectURL(this.files[0]));
 
-  console.log(src);
+  const resolution = computeResolution(image);
+  await putImageToCanvas(image, getCanvasPreviewRaw(), resolution);
 
-  await putImageToCanvas(src, getCanvas());
+  const ditheredImageData: ImageData = floydSteinberg(
+    getCanvasPreviewRaw()
+      .getContext("2d")
+      .getImageData(0, 0, resolution.width, resolution.height)
+  );
+
+  getCanvasPreviewDithered().width = resolution.width;
+  getCanvasPreviewDithered().height = resolution.height;
+  getCanvasPreviewDithered()
+    .getContext("2d")
+    .putImageData(ditheredImageData, 0, 0);
+
+  document.querySelector("#output").innerHTML = await tenjify(
+    getCanvasPreviewDithered().toDataURL()
+  );
+}
+
+function copyToClipboard() {
+  const output = document.querySelector("#output") as HTMLTextAreaElement;
+  output.select();
+  document.execCommand("copy");
 }
 
 // entrypoint
