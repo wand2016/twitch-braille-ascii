07185859c786c4eafaa4d85c709dfd808136578b
wand2016 <d19921207@gmail.com>
2021-08-13 02:49:42 +0900

    publish

---
+++ vue.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  pages: {
+    index: {
+      // entry for the page
+      entry: "src/main.ts",
+      // the source template
+      template: "template/index.html",
+      // output as dist/index.html
+      filename: "index.html",
+      // chunks to include on this page, by default includes
+      // extracted common chunks and vendor chunks.
+      chunks: ["chunk-vendors", "chunk-common", "index"],
+    },
+  },
+};
